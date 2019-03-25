ff32250e1fcfcf928c4ad24b8d4d489547e95a72
Aleksandra Borowska <aleks.borowska91@gmail.com>
2019-03-25 16:00:57 +0100

    Updates

---
+++ precache-manifest.fadf6295c97982128557be9b277759b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56cc1497069633104c46",
+    "url": "/video_player/static/js/runtime~main.982e2b45.js"
+  },
+  {
+    "revision": "d6f7fbab30128cc74721",
+    "url": "/video_player/static/js/main.24e33ba5.chunk.js"
+  },
+  {
+    "revision": "5d21101413ea9baee411",
+    "url": "/video_player/static/js/2.589c286c.chunk.js"
+  },
+  {
+    "revision": "d6f7fbab30128cc74721",
+    "url": "/video_player/static/css/main.80ed4e79.chunk.css"
+  },
+  {
+    "revision": "3a45221147382e93e6753e9abde5beb1",
+    "url": "/video_player/index.html"
+  }
+];
